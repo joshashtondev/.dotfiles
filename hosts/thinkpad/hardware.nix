54e641cf0ab8fb9159dab66c8acb6c60571cf0e1
joshashtondev <me@joshashton.dev>
2026-07-22 23:35:51 -0600

    nixos: gen 115 @ nix-pad — 2026-07-22 23:35
    26.05.20260717.293d6ab (Yarara)

--- hosts/thinkpad/hardware.nix
+++ hosts/thinkpad/hardware.nix
@@ -11,6 +11,12 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" "uvcvideo" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.blacklistedKernelModules = [ 
+    "intel_ipu6" 
+    "intel_ipu6_isys" 
+    "intel_ipu6_md"
+  ];
+
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
