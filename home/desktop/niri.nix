b9224043e35229d2af7c182800752dc26f68cc6a
joshashtondev <me@joshashton.dev>
2026-07-30 23:58:13 -0600

    nixos: gen 170 @ nix-pad — 2026-07-30 23:58 26.05.20260724.597283a (Yarara)

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -13,7 +13,7 @@ let
         -g "3,RL,*,*,P,niri msg action focus-column-right && sleep 0.4" \
         -g "3,UD,*,*,P,niri msg action focus-workspace-up && sleep 0.4" \
         -g "3,DU,*,*,P,niri msg action focus-workspace-down && sleep 0.4" \
-        -g "4,DU,*,*,P,dms ipc spotlight toggle && sleep 0.4"
+        -g "4,DU,*,*,P,dms ipc spotlight toggle && sleep 0.4" \
         -g "2,DU,B,*,pkill -RTMIN wvkbd-mobintl"
     fi
   '';
