f8e13e2c0cf86c60a5b1243cdc35ad9a49e45e29
joshashtondev <me@joshashton.dev>
2026-07-31 21:42:51 -0600

    nixos: gen 203 @ nix-pad — 2026-07-31 21:42 26.05.20260724.597283a (Yarara)

--- home/programs/layout.custom.h
+++ home/programs/layout.custom.h
@@ -1139,10 +1139,11 @@ static struct key keys_landscape[] = {
   {"?123", "?123", 1.0, Layout, 0, &layouts[LandscapeSpecial]},
   {"Ctr", "Ctr", 1.0, Mod, Ctrl, .scheme = 1},
   {",", ",", 1.0, Code, KEY_COMMA, .scheme = 1},
-  {"🙂", "🙂", 1.0, Layout, 0, &layouts[Emoji]},
-  {"", "", 4.0, Code, KEY_SPACE, .scheme = 1},
-  {"Tab", "Tab", 1.0, Code, KEY_TAB, .scheme = 1},
   {".", ".", 1.0, Code, KEY_DOT, .scheme = 1},
+  {"🙂", "🙂", 0.75, Layout, 0, &layouts[Emoji]},
+  {"", "", 4.0, Code, KEY_SPACE, .scheme = 1},
+  {"Tab", "Tab", 0.75, Code, KEY_TAB, .scheme = 1},
+  {"→", "→", 0.75, Code, KEY_RIGHT, .scheme = 1},
   {"Enter", "Enter", 2.0, Code, KEY_ENTER, .scheme = 1},
 
   /* end of layout */
