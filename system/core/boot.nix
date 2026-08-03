8e77897e327147a86eff34ea28ed591b4dcfc2ef
joshashtondev <me@joshashton.dev>
2026-08-02 19:42:05 -0600

    nixos: gen 216 @ nix-pad — 2026-08-02 19:41 26.05.20260724.597283a (Yarara)
    Update to latest kernel.

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -3,7 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "uinput" ];
   system.nixos.label = "niri-v.1.1";
   environment.pathsToLink = [ "/libexec" ];
